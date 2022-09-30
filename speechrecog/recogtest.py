fb622b48e24f58132e6717eb5cd79ca4b9ea7e3c
OmarAlAkkad <omarakkad97@gmail.com>
2022-09-30 13:50:36 -0500

    initial speechrecognition code

--- speechrecog/recogtest.py
+++ speechrecog/recogtest.py
@@ -4,4 +4,12 @@ Created on Fri Sep 23 13:54:39 2022
 
 @author: omars
 """
-import SpeechRecognition
+import speech_recognition as sr
+r = sr.Recognizer()
+
+harvard = sr.AudioFile('audio.wav')
+with harvard as source:
+    audio = r.record(source)
+
+transcript = r.recognize_google(audio)
+print(transcript)
