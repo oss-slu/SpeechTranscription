c09ec633d4eec7c58dc463c24639ca66459c9d21
Jake Wahle <37639498+jakewahle@users.noreply.github.com>
2023-03-07 18:55:20 -0600

    Commented out code relating to diarization. As we are not focused on diarization at the moment due to the difficulties it has been causing us, it is best to comment this out to reduce potential executable size (Resemblyzer particularly has a significant size)

--- functions/diarizationAndTranscription.py
+++ functions/diarizationAndTranscription.py
@@ -1,9 +1,9 @@
-from resemblyzer import VoiceEncoder
-from resemblyzer.audio import sampling_rate
-from spectralcluster import SpectralClusterer
+# Diarization-related libraries
+# from resemblyzer import VoiceEncoder
+# from resemblyzer.audio import sampling_rate
+# from spectralcluster import SpectralClusterer
+# import librosa
 from pydub import AudioSegment
-import speech_recognition
-import librosa
 import whisper
 
 # Adapted from https://github.com/raotnameh/Trim_audio
@@ -12,28 +12,30 @@ import whisper
 # filename: name of wav file
 # 
 # Trims a segment from a wav file, exports to current directory as 'filename'_segment.wav
+"""
 def trim(start, end, filename):
 
     t1 = start * 1000 # converts to milliseconds
     t2 = end * 1000
-    print("t1: " + str(t1))
-    print("t2: " + str(t2))
     trimmedAudio = AudioSegment.from_wav(filename)
     trimmedAudio = trimmedAudio[t1:t2]
-    print("Trimmed audio duration: " + str(trimmedAudio.duration_seconds))
     # Gets name of file without extension
     name = filename.split('.')[0]
     trimmedAudio.export(name + "_segment.wav", format="wav")
-
+"""
+    
 # Formats audio to be accepted by clustering process
+"""
 def resample(audio):
     wav, sampleRate = librosa.load(str(audio), sr=None)
     resampledAudio = librosa.resample(wav, orig_sr=sampleRate, target_sr=sampling_rate)
     return resampledAudio
+"""
 
 # Function adapted from https://medium.com/saarthi-ai/who-spoke-when-build-your-own-speaker-diarization-module-from-scratch-e7d725ee279
 # Returns an array of tuples of form [speakerNumber, startTime, endTime]
 # Effectively identifies segments and the speakers of those segments
+"""
 def create_labelling(labels, wav_splits):
     times = [((s.start + s.stop) / 2) / sampling_rate for s in wav_splits]
     labelling = []
@@ -49,17 +51,11 @@ def create_labelling(labels, wav_splits):
             labelling.append(tuple(temp))
 
     return labelling
+"""
 
 def transcribeAudio(audioFile):
     model = whisper.load_model("base.en")
     transcribedAudio = model.transcribe(audioFile, fp16=False, language='English')
-    # recognizer = speech_recognition.Recognizer()
-    # formattedAudio = speech_recognition.AudioFile(audioFile)
-    # with formattedAudio as source:
-    #     recording = recognizer.record(source)
-    # transcribedAudio = recognizer.recognize_google(recording, language = 'en-IN')
-    print('Transcribing: ', audioFile)
-    # return transcribedAudio
     return transcribedAudio["text"]
 
 def diarizeAndTranscribe(audioFile):
@@ -84,7 +80,6 @@ def diarizeAndTranscribe(audioFile):
     for i in range(len(labelling)):
         transcript += "Speaker " + labelling[i][0] + ": "
         trim(labelling[i][1], labelling[i][2], audioFile)
-        print("Attempting to transcribe: " + audioFile)
         transcript += transcribeAudio(name + "_segment.wav") + "\n"
     """
     transcript = transcribeAudio(audioFile) + "\n"
@@ -94,5 +89,4 @@ def diarizeAndTranscribe(audioFile):
     transcript = transcript.replace('? ', '?\n')
     transcript = transcript.replace('`', "'")
     transcript = transcript.strip()
-    print(transcript)
     return transcript
