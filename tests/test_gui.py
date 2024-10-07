bbdd51859eb55d571b31264f57fd71df5a7fe73a
Emra Meduseljac <emmy@Emras-MacBook-Air.local>
2024-10-07 08:56:34 -0500

    adding path, still including comment about running pythonpath b/c i need both

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,5 +1,8 @@
 import pytest
 from GUI import *
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 # run export PYTHONPATH=$(pwd) in terminal
 
 @pytest.fixture
