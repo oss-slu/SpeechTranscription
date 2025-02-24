0ee06472ea53e94ab613e1c2c4c9619e5dabadae
Krishna Pothuganti <kspothuganti@gmail.com>
2025-02-24 15:54:48 -0600

    123

---
+++ tests/test_removeErrorCoding.py
@@ -0,0 +1,26 @@
+import pytest
+import addConventions
+#or run export PYTHONPATH=$(pwd) before running python tests/saltify_test.py
+
+
+def test_one():
+    x = "(I I) I telled[EO:told] a lie."
+    assert addConventions.removeErrorCoding(x) == "I told a lie."
+def test_two():
+    x = "He walking[EW:walked] on the sidewalk."
+    assert addConventions.removeErrorCoding(x) == "He walked on the sidewalk."
+def test_three():
+    x = "This is a normal sentence with no error coding."
+    assert addConventions.removeErrorCoding(x) == "This is a normal sentence with no error coding."
+def test_four():
+    x = "He liked his[EP:her] shoes."
+    assert addConventions.removeErrorCoding(x) == "He liked her shoes."
+def test_five():
+    x = "He looked at[EW] sad."
+    assert addConventions.removeErrorCoding(x) == "He looked sad."
+def test_six():
+    x = "Give it *to me."
+    assert addConventions.removeErrorCoding(x) == "Give it to me." 
+def test_seven():
+    x = "The car go/*3s fast."
+    assert addConventions.removeErrorCoding(x) == "The car goes fast."
