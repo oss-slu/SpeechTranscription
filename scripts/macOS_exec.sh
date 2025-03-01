e58db9d49f2bca66f35c3b5a725c3b0d07d07006
Manali Gaikwad <manaligaikwad135@gmail.com>
2025-02-28 21:15:59 -0600

    Adding BASE_DIR variable

--- scripts/macOS_exec.sh
+++ scripts/macOS_exec.sh
@@ -1,20 +1,25 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+BASE_DIR="$SCRIPT_DIR/.."
+
 pyinstaller --onefile --noconsole \
-  --add-data "/Users/ksp/Desktop/temp/SpeechTranscription/images:images" \
-  --add-data "/Users/ksp/Desktop/temp/SpeechTranscription/build_assets/en-model.slp:pattern/text/en" \
-  --add-data "/Users/ksp/Desktop/temp/SpeechTranscription/CTkXYFrame:CTkXYFrame" \
+  --add-data "$BASE_DIR/images:images" \
+  --add-data "$BASE_DIR/build_assets/en-model.slp:pattern/text/en" \
+  --add-data "$BASE_DIR/CTkXYFrame:CTkXYFrame" \
   --add-binary "/opt/homebrew/opt/portaudio/lib/libportaudio.2.dylib:." \
   --add-binary "/opt/homebrew/bin/ffmpeg:." \
   --add-binary "/opt/homebrew/bin/ffprobe:." \
-  --add-data "/Users/ksp/Desktop/temp/SpeechTranscription/myenv/lib/python3.11/site-packages/lightning_fabric:lightning_fabric" \
-  --add-data "/Users/ksp/Desktop/temp/SpeechTranscription/myenv/lib/python3.11/site-packages/whisper:whisper" \
-  --add-data "/Users/ksp/Desktop/temp/SpeechTranscription/myenv/lib/python3.11/site-packages/filelock:filelock" \
-  --add-data "/Users/ksp/Desktop/temp/SpeechTranscription/myenv/lib/python3.11/site-packages/lightning_fabric/version.info:lightning_fabric" \
-  --add-data "/Users/ksp/Desktop/temp/SpeechTranscription/myenv/lib/python3.11/site-packages/pytorch_lightning:torchlightning" \
-  --add-data "/Users/ksp/Desktop/temp/SpeechTranscription/myenv/lib/python3.11/site-packages/pyannote:pyannote" \
+  --add-data "$BASE_DIR/myenv/lib/python3.11/site-packages/lightning_fabric:lightning_fabric" \
+  --add-data "$BASE_DIR/myenv/lib/python3.11/site-packages/whisper:whisper" \
+  --add-data "$BASE_DIR/myenv/lib/python3.11/site-packages/filelock:filelock" \
+  --add-data "$BASE_DIR/myenv/lib/python3.11/site-packages/lightning_fabric/version.info:lightning_fabric" \
+  --add-data "$BASE_DIR/myenv/lib/python3.11/site-packages/pytorch_lightning:torchlightning" \
+  --add-data "$BASE_DIR/myenv/lib/python3.11/site-packages/pyannote:pyannote" \
   --hidden-import "lightning_fabric" \
   --hidden-import "torch" \
   --hidden-import "torchvision" \
   --hidden-import "pytorch_lightning" \
   --hidden-import "pyannote.audio" \
-  /Users/ksp/Desktop/temp/SpeechTranscription/GUI.py
-  
+  "$BASE_DIR/GUI.py"
