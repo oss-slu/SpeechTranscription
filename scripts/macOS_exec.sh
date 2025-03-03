46939f462ec494279eb9030f2739ab699d89afea
Manali Gaikwad <manaligaikwad135@gmail.com>
2025-03-03 17:17:13 -0600

    Use VENV_NAME variable

--- scripts/macOS_exec.sh
+++ scripts/macOS_exec.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
 BASE_DIR="$SCRIPT_DIR/.."
 
-pyinstaller --onefile --noconsole \
+VENV_NAME=$(basename "$VIRTUAL_ENV")
+
+pyinstaller --onefile --windowed \
   --add-data "$BASE_DIR/images:images" \
   --add-data "$BASE_DIR/build_assets/en-model.slp:pattern/text/en" \
   --add-data "$BASE_DIR/CTkXYFrame:CTkXYFrame" \
   --add-binary "/opt/homebrew/opt/portaudio/lib/libportaudio.2.dylib:." \
   --add-binary "/opt/homebrew/bin/ffmpeg:." \
   --add-binary "/opt/homebrew/bin/ffprobe:." \
-  --add-data "$BASE_DIR/myenv/lib/python3.11/site-packages/lightning_fabric:lightning_fabric" \
-  --add-data "$BASE_DIR/myenv/lib/python3.11/site-packages/whisper:whisper" \
-  --add-data "$BASE_DIR/myenv/lib/python3.11/site-packages/filelock:filelock" \
-  --add-data "$BASE_DIR/myenv/lib/python3.11/site-packages/lightning_fabric/version.info:lightning_fabric" \
-  --add-data "$BASE_DIR/myenv/lib/python3.11/site-packages/pytorch_lightning:torchlightning" \
-  --add-data "$BASE_DIR/myenv/lib/python3.11/site-packages/pyannote:pyannote" \
+  --add-data "$BASE_DIR/$VENV_NAME/lib/python3.11/site-packages/lightning_fabric:lightning_fabric" \
+  --add-data "$BASE_DIR/$VENV_NAME/lib/python3.11/site-packages/whisper:whisper" \
+  --add-data "$BASE_DIR/$VENV_NAME/lib/python3.11/site-packages/filelock:filelock" \
+  --add-data "$BASE_DIR/$VENV_NAME/lib/python3.11/site-packages/pytorch_lightning:torchlightning" \
+  --add-data "$BASE_DIR/$VENV_NAME/lib/python3.11/site-packages/pyannote:pyannote" \
   --hidden-import "lightning_fabric" \
   --hidden-import "torch" \
   --hidden-import "torchvision" \
