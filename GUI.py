962f727968ffff7c1b24afc380e0f6a73243d8b7
Risha Bhosekar <rbhosekar@slu.edu>
2025-11-16 19:34:52 -0600

    working

--- GUI.py
+++ GUI.py
@@ -3,6 +3,7 @@ import logging
 import os
 import sys
 import nltk # type: ignore
+import subprocess
 
 # Ensure NLTK knows where to find the bundled data when running as a frozen app
 app_dir = os.path.dirname(os.path.abspath(__file__))
